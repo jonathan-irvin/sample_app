9bc9cb2d1646b38529b16b543e8da13160b1d158
Jonathan Irvin <djfoxyslpr@gmail.com>
2015-05-26 15:54:05 -0500

    Saving work for now

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -12,6 +12,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/new'
   end
+
   test "valid signup information" do 
   	get signup_path
   	assert_difference 'User.count', 1 do 
@@ -19,7 +20,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   											  email: "user@example.com",
   											  password: "password",
   											  password_confirmation: "password" }
-	end
+	  end
 	assert_template 'users/show'
+  assert is_logged_in?
   end
+  
 end
